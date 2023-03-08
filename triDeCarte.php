54098cf57d9a65e07cd0ca9cb42deeb8349a3ce1
Elias PODER <elias.poderr@gmail.com>
2023-03-08 16:20:36 +0100

    save

---
+++ triDeCarte.php
@@ -0,0 +1,28 @@
+<?php
+
+class Carte {
+    public $couleur;
+    public $valeur;
+    function comparerCouleur($c){
+        if(this)
+    }
+}
+
+class Main {
+    public $listeCarte;
+}
+
+function comparerCouleur(){
+
+}
+
+function comparerValeur(){
+
+}
+
+
+
+$main->listeCarte = $listeCarteValeur;
+
+
+
